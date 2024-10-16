d16747e9f7d3495cf1e07d4fb4b483eaf61f486d
Richard Viney <richard.viney@gmail.com>
2024-10-17 11:39:53 +1300

    chore: updates for automatic release process

--- Formula/dcmfx.rb
+++ Formula/dcmfx.rb
@@ -1,6 +1,13 @@
 VERSION = "0.1.0"
 URL_PREFIX = "https://github.com/dcmfx/dcmfx/releases/download/v#{VERSION}/"
 
+SHAS = {
+  "aarch64-apple-darwin" => "d3f3762b1bc947146390b564708243cbcc383820bae75aac639b261bcc40658d",
+  "x86_64-apple-darwin" => "2234d7aa5affb7cb4ef4efc9f16de2f16ac8858c868fc36b2f78b8bfe7508f5e",
+  "aarch64-unknown-linux-musl" => "479ffcfd83186da09f6e337c148f83f147ad45931b5e469a912a5c2ab9600944",
+  "x86_64-unknown-linux-musl" => "8a957132eee10cf69434c08851aee9fa1a8a6ede850377cfcaef7011ab9e642c"
+}
+
 class Dcmfx < Formula
   desc "DCMfx - CLI tool for working with DICOM and DICOM JSON files"
   homepage "https://github.com/dcmfx"
@@ -10,20 +17,20 @@ class Dcmfx < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "#{URL_PREFIX}dcmfx-cli-v#{VERSION}-aarch64-apple-darwin.tar.gz"
-      sha256 "d3f3762b1bc947146390b564708243cbcc383820bae75aac639b261bcc40658d"
+      sha256 SHAS.fetch("aarch64-apple-darwin")
     else
       url "#{URL_PREFIX}dcmfx-cli-v#{VERSION}-x86_64-apple-darwin.tar.gz"
-      sha256 "2234d7aa5affb7cb4ef4efc9f16de2f16ac8858c868fc36b2f78b8bfe7508f5e"
+      sha256 SHAS.fetch("x86_64-apple-darwin")
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "#{URL_PREFIX}dcmfx-cli-v#{VERSION}-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "479ffcfd83186da09f6e337c148f83f147ad45931b5e469a912a5c2ab9600944"
+      sha256 SHAS.fetch("aarch64-unknown-linux-musl")
     else
       url "#{URL_PREFIX}dcmfx-cli-v#{VERSION}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "8a957132eee10cf69434c08851aee9fa1a8a6ede850377cfcaef7011ab9e642c"
+      sha256 SHAS.fetch("x86_64-unknown-linux-musl")
     end
   end
 
